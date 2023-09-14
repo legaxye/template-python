be33ecd98c2f0c884b97fcc9ae674e2d0cfc2131
legaxye <81601255+legaxye@users.noreply.github.com>
2023-09-14 10:05:57 -0500

    Update server.py
    Signed-off-by: legaxye <81601255+legaxye@users.noreply.github.com>

--- server.py
+++ server.py
@@ -1,47 +1,47 @@
 import os
-from flask import Flask, send_from_directory, render_template, redirect
-#ini
-from flask import Blueprint, jsonify
+from flask import Flask, send_from_directory, render_template, redirect, jsonify
 from threading import Thread
 import time
 import multiprocessing
-#end
 
 app = Flask(__name__)
 
 port = int(os.environ.get("PORT", 5000))
 
-#ini
-contador = multiprocessing.Value('i', 0)
+contador = multiprocessing.Value('i', 0)  # Crear un valor compartido para el contador
+contador_queue = multiprocessing.Queue()  # Crear una cola para comunicar el valor del contador
+
 def incrementar_contador():
     global contador
     while True:
         with contador.get_lock():
             contador.value += 10
-        time.sleep(10)
+            contador_queue.put(contador.value)  # Agregar el valor actual del contador a la cola
+        time.sleep(1)
+
 thread = Thread(target=incrementar_contador)
-#end
+
+@app.route('/')
+def index():
+    return "<p>Hello, World, me llamou jusss!1</p>"
+
+@app.route('/add/<username>')
+def add_user(username):
+    return "<p>Hello, " + username + ", me llamou jseeu!</p>"
 
 @app.route('/mostrar_num')
 def obtener_contador():
     with contador.get_lock():
         return jsonify({'contador': contador.value})
-        
-@app.route('/add/<username>')
-def add_user(username):
-    return "<p>Hello, " + username + ", me llamou jseeu!</p>"
-    
+
 @app.route('/static/<path:path>')
 def serve_static(path):
     return send_from_directory('static', path)
-
-@app.route('/')
-def home():
-   return "<p>Hello, World, me llamou jusss!</p>"
 
 @app.route('/<path:path>')
 def all_routes(path):
     return redirect('/')
 
 if __name__ == "__main__":
+    thread.start()  # Iniciar el hilo para incrementar el contador
     app.run(port=port)
