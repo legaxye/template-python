6d295d7ce3874822a37de391ed6041a3c3af9a23
legaxye <81601255+legaxye@users.noreply.github.com>
2023-09-14 10:24:11 -0500

    Update server.py
    Signed-off-by: legaxye <81601255+legaxye@users.noreply.github.com>

--- server.py
+++ server.py
@@ -2,37 +2,29 @@ import os
 from flask import Flask, send_from_directory, render_template, redirect, jsonify
 from threading import Thread
 import time
-import multiprocessing
 
 app = Flask(__name__)
 
 port = int(os.environ.get("PORT", 5000))
 
-contador = multiprocessing.Value('i', 0)  # Crear un valor compartido para el contador
-contador_queue = multiprocessing.Queue()  # Crear una cola para comunicar el valor del contador
+contador = 0
 
 def incrementar_contador():
     global contador
     while True:
-        with contador.get_lock():
-            contador.value += 10
-            contador_queue.put(contador.value)  # Agregar el valor actual del contador a la cola
-        time.sleep(1)
+        contador += 10
+        time.sleep(1)  # Incrementar el contador cada 10 segundos
 
 thread = Thread(target=incrementar_contador)
+thread.start()  # Iniciar el hilo para incrementar el contador
 
 @app.route('/')
 def index():
-    return "<p>Hello, World, me llamou jusss!1</p>"
+    return "<p>Hello, World, me llamou jusemarias10!</p>"
 
-@app.route('/add/<username>')
-def add_user(username):
-    return "<p>Hello, " + username + ", me llamou jseeu!</p>"
-
-@app.route('/mostrar_num')
+@app.route('/obtener_contador')
 def obtener_contador():
-    with contador.get_lock():
-        return jsonify({'contador': contador.value})
+    return jsonify({'contador': contador})
 
 @app.route('/static/<path:path>')
 def serve_static(path):
@@ -43,5 +35,4 @@ def all_routes(path):
     return redirect('/')
 
 if __name__ == "__main__":
-    thread.start()  # Iniciar el hilo para incrementar el contador
     app.run(port=port)
