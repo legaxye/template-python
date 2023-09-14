f61517e93cb1bc1a1823a4329c1fd1aff05413b4
legaxye <81601255+legaxye@users.noreply.github.com>
2023-09-14 09:56:20 -0500

    Update server.py
    Signed-off-by: legaxye <81601255+legaxye@users.noreply.github.com>

--- server.py
+++ server.py
@@ -1,12 +1,24 @@
 import os
 from flask import Flask, send_from_directory, render_template, redirect
+#ini
+from flask import Blueprint, jsonify
+from threading import Thread
+import time
+import multiprocessing
+#end
 
 app = Flask(__name__)
 
 port = int(os.environ.get("PORT", 5000))
 
+#ini
+contador = multiprocessing.Value('i', 0)
+#end
 
-
+@app.route('/add/<username>')
+def add_user(username):
+    return "<p>Hello, " + username + ", me llamou jseeu!</p>"
+    
 @app.route('/static/<path:path>')
 def serve_static(path):
     return send_from_directory('static', path)
