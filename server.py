00f175dc69691805e6590e7b6f187e9a085538ef
legaxye <81601255+legaxye@users.noreply.github.com>
2023-09-14 09:49:55 -0500

    Update server.py
    Signed-off-by: legaxye <81601255+legaxye@users.noreply.github.com>

--- server.py
+++ server.py
@@ -1,37 +1,11 @@
 import os
 from flask import Flask, send_from_directory, render_template, redirect
-#ini
-from flask import Blueprint, jsonify
-from threading import Thread
-import time
-import multiprocessing
-#end
+
 app = Flask(__name__)
 
 port = int(os.environ.get("PORT", 5000))
 
-#ini
-contador = multiprocessing.Value('i', 0) # Crear un valor compartido para el contador
-def incrementar_contador():
-    global contador
-    while True:
-        with contador.get_lock():
-            contador.value += 10
-        time.sleep(10)
-thread = Thread(target=incrementar_contador)
-@app.route('/')
-def index():
-    return "<p>Hello, World, me llamou jusss!</p>"
 
-@app.route('/add/<username>')
-def add_user(username):
-    return "<p>Hello, " + username + ", me llamou jseeu!</p>"
-
-@app.route('/mostrar_num')
-def obtener_contador():
-    with contador.get_lock():
-        return jsonify({'contador': contador.value})
-#end
 
 @app.route('/static/<path:path>')
 def serve_static(path):
